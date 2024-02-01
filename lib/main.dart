ed086de99b9262cffdaa22cf1bd303f7c96f1a63
minyoung10 <22000374@handong.ac.kr>
2024-02-01 10:18:11 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
       home: HomePage(),
       theme: ThemeData(
           brightness: Brightness.dark,
-          primaryColor: Color.fromRGBO(54, 209, 0, 1),
+          primaryColor: const Color.fromRGBO(54, 209, 0, 1),
           scaffoldBackgroundColor: Colors.white),
       initialRoute: '/login',
       routes: {
